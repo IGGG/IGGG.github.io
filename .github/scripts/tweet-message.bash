f16b18f1f99699942f5d91d3f0f46ea46ae2a40e
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2019-10-14 18:26:59 +0900

    CI: add tweet action

--- .github/scripts/tweet-message.bash
+++ .github/scripts/tweet-message.bash
@@ -6,7 +6,7 @@ BASE_URL="https://iggg.github.io"
 TWEET_MESSAGE=""
 LATEST=0
 
-DIFF_FILES=`git diff HEAD^...HEAD --name-only --diff-filter=A -- source/_posts/*.md`
+DIFF_FILES=`git diff ${TARGET_BRANCH} --name-only --diff-filter=A -- source/_posts/*.md`
 for FILE_PATH in $DIFF_FILES ; do
   TITLE=`head ${FILE_PATH} | grep '^title:' | sed 's/^title: *//g'`
   DATE=`head ${FILE_PATH} | grep '^date:' | sed -e 's/^date: *\([0-9]\{4\}\)-\([0-9]\{2\}\)-\([0-9]\{2\}\) .*$/\1\/\2\/\3/g'`
